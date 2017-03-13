d1918e21c01f58d460b0b5432268ee0abdee574a
kadiray karakaya <kadiray.k@hotmail.com>
2017-03-13 15:41:31 +0200

    Update models.py

--- welcome/models.py
+++ welcome/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class PageView(models.Model):
     hostname = models.CharField(max_length=32)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class Developer(models.Model):
+    name = models.CharField(max_length=64)
+    isNobetci = models.BooleanField()
