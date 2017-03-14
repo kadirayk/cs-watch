371daaad7e7c01a13c5a7f5a5559926d569d35d5
kadiray karakaya <kadiray.k@hotmail.com>
2017-03-14 07:41:14 +0200

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -7,42 +7,44 @@ from . import database
 from .models import PageView
 from .models import Developer
 
+
 # Create your views here.
 
 def index(request):
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
-
     return render(request, 'welcome/index.html', {
-        'hostname': hostname,
-        'database': database.info(),
-        'count': PageView.objects.count()
+        'developers': Developer.objects.all().order_by('name')
     })
+
 
 def watchlist(request):
     return render(request, 'welcome/watchlist.html', {
-        'developers': Developer.objects.all()
+        'developers': Developer.objects.all().order_by('name')
     })
+
 
 def addDeveloper(request):
     d = Developer(name=request.GET["name"])
     d.save()
     return render(request, 'welcome/watchlist.html', {
-        'developers': Developer.objects.all()
+        'developers': Developer.objects.all().order_by('name')
     })
+
 
 def deleteDeveloper(request):
     d = Developer.objects.filter(name__contains=request.GET["name"])
     d.delete()
     return render(request, 'welcome/watchlist.html', {
-        'developers': Developer.objects.all()
+        'developers': Developer.objects.all().order_by('name')
     })
 
+
 def nobetcify(request):
+    Developer.objects.all().update(isNobetci=False)
     Developer.objects.filter(name__contains=request.GET["name"]).update(isNobetci=True)
     return render(request, 'welcome/watchlist.html', {
-        'developers': Developer.objects.all()
+        'developers': Developer.objects.all().order_by('name')
     })
+
 
 def health(request):
     return HttpResponse(PageView.objects.count())
