96abd32555d3751a3acffcda9a630e548b407222
kadiray karakaya <kadiray.k@hotmail.com>
2017-03-17 20:26:25 +0200

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health, addDeveloper, deleteDeveloper, nobetcify, audit, sendMail
+from welcome.views import index, health, addDeveloper, deleteDeveloper, nobetcify, audit, sendMail, addMeta, deleteMeta
 
 urlpatterns = [
     # Examples:
@@ -11,9 +11,11 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^deleteDeveloper$', deleteDeveloper),
     url(r'^audit$', audit),
-    url(r'^sendMail$', sendMail),
+    url(r'^sendMail', sendMail),
     url(r'^addDeveloper$', addDeveloper),
     url(r'^nobetcify$', nobetcify),
+    url(r'^addMeta$', addMeta),
+    url(r'^deleteMeta$', deleteMeta),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
 ]
